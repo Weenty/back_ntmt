21fb837b01c0f5ab8e2e2e7dcc0135e0f02c00a8
Weenty <flaky12r@mail.ru>
2023-05-11 18:57:03 +0500

    add get users by group route

--- handlers/groups/handler.js
+++ handlers/groups/handler.js
@@ -26,6 +26,36 @@ async function getGroups(object, user) {
   return data;
 }
 
+async function getStudentsByGroup(group, user) {
+  let data = {
+    message: "",
+    statusCode: 400,
+  };
+  const client = await pool.connect();
+  try {
+    const resQueryGetUsers = await client.query(`SELECT u."id", concat_ws(' ', b."secondName", b."name", b."patronomyc") as "fio"
+                                                  FROM users u
+                                                  left join bios b on u."id" = b.id
+                                                  WHERE u."groupId" = $1`, [group])
+    data = {
+      message: resQueryGetUsers.rows,
+      statusCode: 200,
+    };
+    return data;
+  } catch (e) {
+    console.log(e);
+    data = {
+      message: e,
+      statusCode: 400,
+    };
+  } finally {
+    client.release();
+    console.log("client.release");
+  }
+  return data;
+}
+
 module.exports = {
   getGroups: getGroups,
+  getStudentsByGroup:getStudentsByGroup
 };
--- routes/groups/route.js
+++ routes/groups/route.js
@@ -23,5 +23,30 @@ module.exports = function (fastify, opts, next) {
             }
         }
     })
+
+    fastify.route({
+        method: "GET",
+        url: "/:group",
+        schema: {
+          params: {
+            type: "object",
+            properties: {
+              id: { type: ["integer", "string"] },
+            },
+          },
+          tags: ["groups"],
+        },
+        async handler(request, reply) {
+          const group = request.params.group;
+          const data = await job.getStudentsByGroup(group, request.info);
+          if (data.statusCode == 200) {
+            reply.status(200);
+            return data;
+          } else {
+            reply.status(400);
+            return data;
+          }
+        },
+      });
     next()
 }
