f2b67914ca86828b667a8ca7faef9c88e39a7652
Weenty <flaky12r@mail.ru>
2023-05-23 21:47:24 +0500

    f

--- services/constants.js
+++ services/constants.js
@@ -2,12 +2,7 @@ const LOGIN_TYPES = {
     activeDirectory: 1,
     loginPassword: 2
 }
-const USER_ROLES = {
-    admin: 1,
-    teacher: 2,
-    student: 3,
-    parent: 4
-}
+
 const TYPES_OF_STUDYING = {
     fullTime: 1, //очка
     extramural: 2 //заочка
@@ -41,7 +36,6 @@ const MIME_IMAGES = [
 
 module.exports = {
     LOGIN_TYPES: LOGIN_TYPES,
-    USER_ROLES: USER_ROLES,
     TYPES_OF_STUDYING: TYPES_OF_STUDYING,
     TYPES_OF_EXAM: TYPES_OF_EXAM,
     TYPES_OF_SEMESTERS: TYPES_OF_SEMESTERS,
--- services/libs/ldap.js
+++ services/libs/ldap.js
@@ -1,4 +1,4 @@
-const ldap = require('ldapjs')
+// const ldap = require('ldapjs')
 
 // const ldapClient = ldap.createClient({
 //     url:'ldap://172.16.0.10:389'
